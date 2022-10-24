1ab50d46bd7307136379be8000c9241f6e1ccd47
mazenbeik1 <58854840+mazenbeik1@users.noreply.github.com>
2022-10-25 01:15:53 +0300

    Add files via upload

---
+++ ExpertSys.js
@@ -0,0 +1,234 @@
+//MODEL
+const name=0;
+const country=[
+                ["Egypt","Arabic","Africa","All"],
+                ["Palestine","Arabic","Asia","All"],
+                ["SaudiArabia","Arabic","Asia",],
+                ["Turkey","Turkish","Asia","All"],
+                ["UnitedKingdom","English","Europe","All"],
+                ["America","English","NorthAmerica","All"]
+                ]
+
+const checkBox=[
+    "Africa",
+    "Asia",
+    "Australia",
+    "Europe",
+    "NorthAmerica",
+    "SouthAmerica",
+    "English",
+    "Arabic",
+    "Turkish",
+    "Spanish",
+    "Russian",
+    "Hindi",
+    "Forest",
+    "Desert",
+    "Sea"
+]
+
+let adjs=[]
+
+let idCounter=2
+
+let other=[]
+
+let selectedCountry=[]
+
+
+function selectCountry(filter)
+{
+    country.forEach(obj =>
+    {
+        obj.forEach(key=>{
+            if(key==filter)
+            {
+                selectedCountry.push(obj);
+            }
+        })
+    })
+}
+
+
+function createOther(key)
+{
+    other.push({name:key,id:idCounter++});
+    renderOther();
+}
+
+function removeOther(id)
+{
+    other=other.filter(obj=>
+        {
+                return obj.id!==id;
+        })
+        renderOther();
+}
+
+function addToAdjs(adj)
+{
+    adjs=adjs.filter(elem=>
+        {
+            return elem!==adj;
+        })
+        adjs.push(adj);
+
+        
+        
+}
+
+function restartAdjs()
+{
+    adjs=new Array();
+    
+    other.forEach(elem=>
+        {
+            adjs.push(elem.name);
+        })
+
+}
+
+function check()
+{
+    checkBox.forEach(filter =>
+        {
+            if(document.getElementById(filter).checked)
+            {
+                addToAdjs(filter);   
+            }
+    
+            
+        });
+}
+
+function infer(inference,rule1,rule2="All")
+{
+    
+    country.forEach(obj=>
+        {
+            let checkInf=0;
+            let checkRule1=0;
+            let checkRule2=0;
+            obj.forEach(adj=>
+            {
+                if(adj==inference){checkInf=1;}
+                if(adj==rule1){checkRule1=1;}
+                if(adj==rule2){checkRule2=1;}
+            })
+            
+            if(!checkInf && checkRule1 && checkRule2)
+            {
+                obj.push(inference);
+            }
+        })
+
+    
+}
+//CONTROLLER
+
+function Search()
+{
+    selectedCountry= new Array();
+    restartAdjs();
+    check();
+
+    adjs.forEach(adj=>
+        {
+            selectCountry(adj);
+        })
+    render();
+}
+
+function addButton()
+{
+    let val=document.getElementById("OtherInput").value;
+    let checker=1;
+    other.forEach(key=>{
+        if(key.name==val)
+        {
+            checker=0;
+        }
+    })
+    if(checker)
+    {
+        createOther(val);
+    }
+}
+
+function removeButton(id)
+{
+    removeOther(id);
+}
+
+function update()
+{
+    let inference;
+    if(document.getElementById("Inference").value=="")
+    {
+        alert("Please Insert Inference");
+    }else if(document.getElementById("FirstRule").value!=="" && document.getElementById("SecondRule").value!=="")
+    {
+        let rule1=document.getElementById("FirstRule").value;
+        let rule2=document.getElementById("SecondRule").value;
+        inference=document.getElementById("Inference").value
+        infer(inference,rule1,rule2);
+    }else if(document.getElementById("FirstRule").value=="" || document.getElementById("SecondRule").value=="")
+    {
+        let rule;
+        if(document.getElementById("FirstRule").value=="")
+        {
+            rule=document.getElementById("SecondRule").value;
+        }else if(document.getElementById("SecondRule").value=="")
+        {
+            rule=document.getElementById("FirstRule").value;
+        }
+        inference=document.getElementById("Inference").value;
+        infer(inference,rule);
+    }else
+    {
+        alert("fuck off little ass");
+    }
+}
+
+//VIEW
+function render()
+{
+    renderOther();
+    renderOutput();
+}
+
+function renderOutput()
+{
+    document.getElementById("Output").innerHTML="";
+    selectedCountry.forEach(obj =>
+    {
+        let nameElement=document.createElement("div");
+        nameElement.innerText=obj[name];
+        nameElement.style.fontWeight="bolder";
+        document.getElementById("Output").appendChild(nameElement);
+    })
+}
+
+function renderOther()
+{
+    document.getElementById("OtherList").innerHTML="";
+    other.forEach(function(key)
+    {
+        let otherElement=document.createElement("div");
+        otherElement.id=key.id;
+        otherElement.style="display:flex";
+        document.getElementById("OtherList").appendChild(otherElement);
+        
+        let otherKey=document.createElement("div");
+        otherKey.innerText=key.name;
+        otherKey.id=key.id;
+        document.getElementById(key.id).appendChild(otherKey);
+
+        let otherButton=document.createElement("button");
+        otherButton.innerText="X";
+        otherButton.id=key.id;
+        otherButton.onclick=function(){removeButton(parseInt(this.id));}
+        document.getElementById(key.id).appendChild(otherButton);
+
+    });
+}
